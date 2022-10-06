3f0b7b4339bdd333894f128cfb245b584b7df800
Webprodev0425 <newstar01@dreampotential.org>
2022-10-06 22:50:06 +0800

    ajax post request sent after the form submission

--- wp-content/themes/astra/assets/js/unminified/custom.js
+++ wp-content/themes/astra/assets/js/unminified/custom.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
     console.log("Custom JS")
+    //timer
     var timer = setInterval(() => {
+        // select an element in an iframe
         if($("#hs-form-iframe-0").contents().find("input.hs-button.primary.large").length > 0){
             clearInterval(timer)
             console.log("Form is loaded")
             $("iframe#hs-form-iframe-0").contents().find("input.hs-button.primary.large").click(function(e){
                 var email = $("iframe#hs-form-iframe-0").contents().find(".hs_email input").val()
                 var fname = $("iframe#hs-form-iframe-0").contents().find(".hs_firstname input").val()
-                var lname = $("iframe#hs-form-iframe-0").contents().find(".hs_lastname input").val()
-                console.log(fname)
-                console.log(lname)
-                console.log(email)
                 var data = {
                     'action': 'my_ajax_request',
                     'post_type': 'POST',
                     'name': fname,
                     'email': email
                   };
+                  //ajax post request send
                 $.post("/wordpress/wp-admin/admin-ajax.php", data, function(response) {
                     console.log( response );
                     }, 'json');
